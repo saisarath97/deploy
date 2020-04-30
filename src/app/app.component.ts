f913b6f656c34bb9c52048eb084d7131982dcccd
saisarath97 <mailsaisarathsetty@gmail.com>
2020-04-30 14:30:47 +0530

    small change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'deploy';
+  title = 'dep ';
+ test:User= new User();
+
+onSubmit()
+{
+  console.log(this.test);
+}
 }
---
+++ src/app/user.ts
@@ -0,0 +1,4 @@
+export class User{
+    Myname="sam";
+    Myname1="s123";
+}
